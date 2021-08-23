914ba86b6af4f1e55bc3845121418e97e79bd7f8
Rajeshwaran <48647716+mypcsregent@users.noreply.github.com>
2021-08-23 18:00:32 +0530

    Add files via upload

--- Q1.js
+++ Q1.js
@@ -1,34 +1,48 @@
-/*1. write a function which has to call 2 different callback functions. 
-The response of first function should be passed to second function 
-and response of second function should be returned as response*/
-
-function Cube_summ(summ)
-{
-    var x= summ*summ*summ;
-    return x;
-}
-
-function Odd_Even(N,callback){
-    if (N%2===0) {
-        console.log("The sum is even, The sum will be halved");
-        return callback(N/2);
-    }
-    else{
-    console.log("The Sum is odd, the sum will be doubled");
-    return callback(N*2);
-    }
-}
-
-function add_no(num1,num2,callback) {
+function Func1(num1,num2,callback){
     var summ=num1+num2;
-    console.log(`Sum of Num1 = ${num1} and Num2 = ${num2} is ${summ} `);
-    return callback(summ,Cube_summ)
+    return callback(summ);
+}
+
+function callbackforF2(z) {
+    console.log(`Result of F2: ${z}`);
+    return z;
+}
+
+function Func2(N,callback) {
+    var Z= N*N;
+    return callback(Z);
 }
 
 
-console.log(`Final Result ${add_no(5,5,Odd_Even)}`)
+function Func3(N,callback) {
+    callback (N*N*N);
+}
 
-//First function finds the sum and calls a callback function which finds whether the sum is even or odd.
-//If the sum is odd then the sum will be doubled. If it is even then will the sum will be halved 
-//and the result will be sent as a response to the second callback function
-//The second function will find the cube.
+function BaseFunction(){
+    var num1=-5, num2=7;
+
+    var ResultOfF1= Func1(num1,num2,(summ)=> {
+        if(summ>=0) console.log(`sum of ${num1} and ${num2} is Positive`);
+        else console.log(`sum of ${num1} and ${num2} is Negative`);
+        return summ;
+    });
+    console.log(`Result of F1: ${ResultOfF1}`);
+
+    var n;
+    if(ResultOfF1>=0) n=1;
+    else n=-1;
+    var N= ResultOfF1*n;
+
+    var ResultofF2=Func2(N,callbackforF2);
+    
+    var N2= ResultOfF1+ResultofF2;
+    var N3;
+    if (N2%2===0) N3=num1;
+    else N3=num2;
+    var X=(N3==num1? num2: num1);
+    console.log(`chosen number: ${N3} instead of ${X}`);
+
+    Func3(N3, (cubes)=>{ console.log(`Result of the third Function : ${cubes}`)});
+}
+
+BaseFunction();
